aabca2f91887101b51fdc6b5a864ac6271244588
Sif Oberon <muell758@d.umn.edu>
2023-11-16 17:18:40 -0600

    clean up comments in app.js, add descriptive comment to index.js describing functionality

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-// Code in this module creates components - reusable code that represents part of the user interface
-// This will render, manage, and update UI elements in the application
+// Code in this module creates components - reusable code blocks that represent part of the UI
+// This renders, manages, and updates those UI elements
 
-// export to make this function accessible outside of this file/module
-// default makes it the main function in the file
+// export: make this function accessible outside of this file/module
+// default: makes it the main function in the file
 export default function Square() {
   // returns a button. <button> is a jsx (javascript xml) element - a combo of JS code & HTML tags
   return <button className="square">X</button>;
