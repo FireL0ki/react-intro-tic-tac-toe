3c2f7bd63f618730fe009af87aac33c110d332f4
Sif Oberon <muell758@d.umn.edu>
2023-11-16 17:13:55 -0600

    add descriptive comments to the initial component function

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+// Code in this module creates components - reusable code that represents part of the user interface
+// This will render, manage, and update UI elements in the application
+
+// export to make this function accessible outside of this file/module
+// default makes it the main function in the file
 export default function Square() {
+  // returns a button. <button> is a jsx (javascript xml) element - a combo of JS code & HTML tags
   return <button className="square">X</button>;
 }
