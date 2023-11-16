dded1825f0e3f0ce9cc0c5327e6067823017bfa0
Sif Oberon <muell758@d.umn.edu>
2023-11-16 17:36:00 -0600

    build Board() default function, initiate Square() function

--- src/App.js
+++ src/App.js
@@ -3,7 +3,30 @@
 
 // export: make this function accessible outside of this file/module
 // default: makes it the main function in the file
-export default function Square() {
+export default function Board() {
   // returns a button. <button> is a jsx (javascript xml) element - a combo of JS code & HTML tags
-  return <button className="square">X</button>;
+  // React components must return a single JSX element. Use Fragments <> </> to wrap multiple JSX elements
+  return (
+    <>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </>
+  );
+}
+
+function Square() {
+
 }
