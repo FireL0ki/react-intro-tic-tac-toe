9bf0bc2002ca21bdca23950f95ca7b25da67cb49
Sif Oberon <muell758@d.umn.edu>
2023-12-19 19:57:31 -0600

    create boolean to track player state, add check if square is already filled / not null

--- src/App.js
+++ src/App.js
@@ -4,18 +4,30 @@ import { useState } from 'react';
 
 // default: makes it the main function in the file
 export default function Board() {
+  // create a boolean to track which player goes next
+  const [xIsNext, setXIsNext] = useState(true);
   // returns a button. <button> is a jsx (javascript xml) element - a combo of JS code & HTML tags
   // React components must return a single JSX element. Use Fragments <> </> to wrap multiple JSX elements
     const [squares, setSquares] = useState(Array(9).fill(null));
 
     function handleClick(i) {
+      // check if the square has already been marked by an 'x' or 'o', ie. it is not null (true)
+      if (squares[i]) {
+        return
+      }
       // create a copy of the squares array using .slice(), following the principals of immutability to maintain data integrity / keep past data intact
       const nextSquares = squares.slice();
-      // add 'X' to the [i] index square of nextSquares array
-      nextSquares[i] = "X";
+      // check which player (x or o) is next and change the square based on that
+      if (xIsNext) {
+        nextSquares[i] = "X";
+      } else {
+        nextSquares[i] = "O";
+      }
+
       // call setSquares to let React know the component's state has changed 
       // This re-renders the components that use the squares state & its child components (Board() & Square(), respectively)
       setSquares(nextSquares);
+      setXIsNext(!xIsNext)
     }
 
     return (
